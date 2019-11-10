52e0bfc7fe928756a229f3bc01e38c9c339b9a37
pnikhare <pnikhare@uncc.edu>
2019-11-10 00:50:09 -0500

    added window & timeout logic

---
+++ Go-Back-N/checksum.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+def carry_around_add(a, b):
+    c = a + b
+    return (c & 0xffff) + (c >> 16)
+
+def computeChecksum(msg):
+    s = 0
+    for i in range(0, len(msg), 2):
+        w = ord(msg[i]) + (ord(msg[i+1]) << 8)
+        s = carry_around_add(s, w)
+    return ~s & 0xffff
+
+def isMsgCorrupted(orig_cksum, msg):
+    if computeChecksum(msg) == orig_cksum:
+        return True
+
+    return False
